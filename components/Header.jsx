cd6b92cabb2a33394374d6f9a66648607b6e062e
Yuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2022-09-14 19:34:42 +0700

    name

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="text-center">
       <p className="display-4 fst-italic">Pixel Painter</p>
-      <p>made by Chayain Suatap 1234567</p>
+      <p>made by Natchanon Nantasri 640612086</p>
     </div>
   );
 }
